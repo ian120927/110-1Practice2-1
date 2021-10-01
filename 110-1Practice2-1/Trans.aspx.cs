1ab26fb11e11c5e085b8966a424e4c1148c9b6fb
哲毅 林 <108111148@mail.aeust.edu.tw>
2021-10-01 19:39:20 +0800

    練習2-1

--- 110-1Practice2-1/Trans.aspx.cs
+++ 110-1Practice2-1/Trans.aspx.cs
@@ -11,10 +11,12 @@ namespace _110_1Practice2_1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            double d_Value, c;
-            d_Value = 66;
-            c = (d_Value - 32) / (9 / 5);
-            Response.Write("d_Value = " + d_Value + "F = " + c + "C");
+            double d_Value = 66;   //設66為華氏溫度
+            double d_Value2 =  0;   //設存放一個攝氏溫度
+
+            d_Value2 = 5.0 * (d_Value - 32.0) / 9.0; //華氏轉攝氏溫度
+
+            Response.Write("d_Value = " + d_Value + "F"  + " = "+ d_Value2+ " C"); //印出結果
         }
     }
 }
